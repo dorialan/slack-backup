6ffa546c79ddb0c607c8d1adc485f16c52c47441
Iurii Marchenko <dorialan@gmail.com>
2016-01-01 23:39:46 +0200

    Basic boostrap layout and pagination

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,18 @@
+class MessagesController < ApplicationController
+  before_action :get_messageable, only: :index
+
+  def index
+    @messages = @messageable.messages.page params[:page]
+    @messageables = Messageable.all
+  end
+
+  private
+
+  def get_messageable
+    @messageable = Messageable.find(messageable_id)
+  end
+
+  def messageable_id
+    params.fetch("messageable_id") { Messageable.first.id }
+  end
+end
